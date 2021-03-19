6b5315372081343ea4925fec7d52f98cb4c2c5f5
johnathanbryce <60417995+johnathanbryce@users.noreply.github.com>
2021-03-19 12:20:42 -0700

    Phase 6
    Nested the endGamePopUp function in the rollFourDiceAndDisplay function due to scope issues when declaring game winner. End game winner declaration javascript is finished, just need to style etc.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,12 +14,15 @@ const $computerDiceTwo = $('#computer-dice-2');
 
 // total scores
 const $playerScoreThisRound = $('#player-score-this-rnd');
-const $playerTotalScore = $('#player-total-score');
+const $playerTotalScoreDisplay = $('#player-total-score');
 const $computerScoreThisRound = $('#computer-score-this-rnd');
-const $computerTotalScore = $('#computer-total-score');
+const $computerTotalScoreDisplay = $('#computer-total-score');
 
 const $countdown = $('#countdown');
 
+const $endGameWinner = $('#end-game-winner');
+
+// create Die objects
 
 class Die {
     constructor(){
@@ -36,101 +39,18 @@ class Die {
     }
 
 
-    updateHTMLToImages(){
-    }
+    
     
 
 }
 
+// track num of rolls for end of game
 let numberOfRolls = 0;
 
 
-//empty arrays to store dice rolls
+// empty arrays to store dice rolls
 playerArray = [ ];
 computerArray = [ ]; 
-
-
-function rollFourDiceAndDisplay(){
-
-    let diceRoll1 = new Die().rollDie();
-    $playerDiceOne.html(diceRoll1);
-
-    let diceRoll2 = new Die().rollDie();
-    $playerDiceTwo.html(diceRoll2);
-
-    let diceRoll3 = new Die().rollDie();
-    $computerDiceOne.html(diceRoll3);
-
-    let diceRoll4 = new Die().rollDie();
-    $computerDiceTwo.html(diceRoll4);
-
-
-    // track number of rolls for winner
-    numberOfRolls++;
-    
-    if(numberOfRolls == 3){
-        endGamePopUp();
-    }
-
-            
-  
-    // player score per round
-    //let zeroFlag = 0;
-    let playerScorePerRound= diceRoll1 + diceRoll2;
-    let computerScorePerRound = diceRoll3 + diceRoll4;
-
-
-    /* rules logic below:
-
-    if(diceRoll1 || diceRoll2 == 1){
-        $playerScoreThisRound.html(`Score This Round: <strong>${zeroFlag} </strong>`);
-    } else{
-        $playerScoreThisRound.html(`Score This Round: <strong>${playerScorePerRound}</strong>`);
-    }
-    */
-
-    $playerScoreThisRound.html(`Score This Round: <strong>${playerScorePerRound}</strong>`);
-    $computerScoreThisRound.html(`Score This Round: <strong> ${computerScorePerRound} </strong>`);
-
-    // push values to the player & computer arrays per dice roll
-    playerArray.push(playerScorePerRound);
-    computerArray.push(computerScorePerRound);
-
-    let playerArraySum = playerArray.reduce(function(a,b){
-        return a + b;
-    }, 0);
-
-    let computerArraySum = computerArray.reduce(function(a,b){
-        return a + b;
-    }, 0);
-    
-    // display the total score of all rounds based on the arrays
-    $playerTotalScore.html(`Total Score: <strong>${playerArraySum}</strong>`);
-    $computerTotalScore.html(`Total Score: <strong>${computerArraySum}</strong>`);
-    
-}
-
-
-function endGamePopUp(){
-    $('#end-game').css('display', 'block');
-
-    let seconds = document.getElementById("countdown").textContent;
-    let countdown = setInterval(function() {
-        seconds--;
-        document.getElementById("countdown").textContent = seconds;
-        if (seconds <= 0) clearInterval(countdown);
-    }, 1000);
-
-
-    setTimeout(function(){
-        location.reload();
-    }, 4000);
-
-
-    
-}
-
-
 
 
 //  -------- game buttons  -----------
@@ -141,7 +61,6 @@ $btnNewGame.click(function(){
     location.reload();
 
 });
-
 
 $btnRules.click(function(){
     $('#rules-pop-up').css('display', 'block')
@@ -161,10 +80,121 @@ $btnCloseEndGame.click(function(){
 
 
 
+// --------  functions ----
+
+
+// ----- roll dice function ------- 
+
+function rollFourDiceAndDisplay(){
+
+    let diceRoll1 = new Die().rollDie();
+    $playerDiceOne.html(diceRoll1);
+
+    let diceRoll2 = new Die().rollDie();
+    $playerDiceTwo.html(diceRoll2);
+
+    let diceRoll3 = new Die().rollDie();
+    $computerDiceOne.html(diceRoll3);
+
+    let diceRoll4 = new Die().rollDie();
+    $computerDiceTwo.html(diceRoll4);
+
+  
+    //  score per round
+    // all 4 dice rolls need their values tested.. so the below code wont work for the score per round due to logic/game rules
+    let playerScorePerRound= diceRoll1 + diceRoll2;
+    let computerScorePerRound = diceRoll3 + diceRoll4;
+
+
+    /* rules logic below:
+
+    if(diceRoll1 || diceRoll2 == 1){
+        $playerScoreThisRound.html(`Score This Round: <strong>${zeroFlag} </strong>`);
+    } else{
+        $playerScoreThisRound.html(`Score This Round: <strong>${playerScorePerRound}</strong>`);
+    }
+    */
+
+    $playerScoreThisRound.html(`Score This Round: <strong>${playerScorePerRound}</strong>`);
+    $computerScoreThisRound.html(`Score This Round: <strong> ${computerScorePerRound} </strong>`);
+
+    // push values to the player & computer arrays per dice roll
+    
+    playerArray.push(playerScorePerRound);
+    computerArray.push(computerScorePerRound);
+
+    // sum the arrays
+    let playerArraySum = playerArray.reduce(function(a,b){
+        return a + b;
+    }, 0);
+
+    let computerArraySum = computerArray.reduce(function(a,b){
+        return a + b;
+    }, 0);
+    
+    // display the total score of all rounds based on the arrays
+
+
+    $playerTotalScoreDisplay.html(`Total Score: <strong>${playerArraySum}</strong>`);
+    $computerTotalScoreDisplay.html(`Total Score: <strong>${computerArraySum}</strong>`);
+
+
+
+    // update die HTML images below 
+
+
+    // end game pop up function  (nested within rollFourDiceAndDisplay due to scope)
+    function endGamePopUp(){
+        $('#end-game').css('display', 'block');
+
+        $('#player-final-score-display').html(`<strong>Your Final Score: ${playerArraySum} </strong>`);
+        $('#computer-final-score-display').html(`<strong> Computer's Final Score: ${computerArraySum} </strong>`);
+    
+       
+        if(playerArraySum  > computerArraySum){
+            $endGameWinner.html(`Congratulations, you have won by ${playerArraySum - computerArraySum} points with a total score of ${playerArraySum}`);
+        }else if(playerArraySum < computerArraySum){
+            $endGameWinner.html(`Uh oh, the Computer has won by ${computerArraySum - playerArraySum} points with a total score of ${computerArraySum}`);
+        } else if (playerArraySum == computerArraySum){
+            $endGameWinner.html(`Wow, we have a tie game -- try again!`);
+        } else {
+            
+        }
+       
+    
+        let seconds = document.getElementById("countdown").textContent;
+        let countdown = setInterval(function() {
+            seconds--;
+            document.getElementById("countdown").textContent = seconds;
+            if (seconds <= 0) clearInterval(countdown);
+        }, 1000);
+    
+    
+        setTimeout(function(){
+            location.reload();
+        }, 5000);
+    
+    
+        
+    }
+    
+
+     // track number of rolls & declare winner
+     numberOfRolls++;
+    
+     if(numberOfRolls == 3){
+         endGamePopUp();
+     }
+
+} // END OF ROLL FUNCTION
 
 
 
 
+// ------- Game Pop-up -------
+    
 
-/
+
+
+
 
